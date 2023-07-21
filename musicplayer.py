497dc0fdbd26b0a3242d99c636c1d062337d17cc
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-07-21 18:57:17 +0600

    Add files via upload

--- musicplayer.py
+++ musicplayer.py
@@ -13,9 +13,9 @@ is_playing = False
 is_paused = False
 total_duration = 0
 current_song = None
-next_song_index = 0
 
 def select_folder():
+    # Function to open a folder selection dialog and load songs from the selected folder.
     folder_selected = filedialog.askdirectory()
     if folder_selected:
         folder_path.set(folder_selected)
@@ -23,6 +23,7 @@ def select_folder():
         load_songs()
 
 def load_songs():
+    # Function to load the list of MP3 songs from the selected folder.
     folder = folder_path.get()
     song_list.delete(0, 'end')
     for file in os.listdir(folder):
@@ -31,7 +32,7 @@ def load_songs():
     song_list.config(width=20)  # Adjust the width as per your preference
 
 def toggle_play(event=None):
-    # Toggle between play and pause based on the current state.
+    # Function to toggle between play and pause based on the current state.
     global is_playing, is_paused, paused_position
 
     if is_playing:
@@ -40,6 +41,7 @@ def toggle_play(event=None):
         paused_position = pygame.mixer.music.get_pos()
         is_playing = False
         is_paused = True
+        play_resume_button.config(image=play_icon)  # Update button image here
     else:
         if is_paused:
             # Resume the song from the paused position
@@ -47,15 +49,17 @@ def toggle_play(event=None):
             pygame.mixer.music.set_pos(paused_position / 1000)  # Set the position in seconds
             is_playing = True
             is_paused = False
+            play_resume_button.config(image=pause_icon)  # Update button image here
         else:
             # Play a new song
             play_song()
             is_playing = True
             is_paused = False
+            play_resume_button.config(image=pause_icon)  # Update button image here
 
 def play_song():
-    # Play the selected song.
-    global current_song, total_duration, is_playing, is_paused, next_song_index
+    # Function to play the selected song.
+    global current_song, total_duration, is_playing, is_paused
 
     is_playing = True
     is_paused = False
@@ -77,11 +81,9 @@ def play_song():
 
         pygame.mixer.music.play()
         play_resume_button.configure(image=pause_icon, command=toggle_play)
-        update_progress()
-        next_song_index = selected_index[0] + 1
 
 def stop_song():
-    # Stop the currently playing song.
+    # Function to stop the currently playing song.
     global is_playing, is_paused
 
     pygame.mixer.music.stop()
@@ -90,45 +92,8 @@ def stop_song():
     play_resume_button.configure(image=play_icon, command=toggle_play)
     progress_label["text"] = "0:00 - 0:00"
 
-def pause_song():
-    # Pause the currently playing song.
-    global is_paused
-
-    pygame.mixer.music.pause()
-    is_paused = True
-    play_resume_button.configure(image=play_icon)
-
-def resume_song():
-    # Resume the paused song.
-    global is_paused
-
-    pygame.mixer.music.unpause()
-    is_paused = False
-    play_resume_button.configure(image=pause_icon)
-
-def play_next():
-    # Play the next song in the list.
-    global next_song_index
-
-    if next_song_index < song_list.size():
-        song_list.selection_clear(0, 'end')
-        song_list.activate(next_song_index)
-        song_list.selection_set(next_song_index)
-        play_song()
-        next_song_index += 1
-
-def play_previous():
-    # Play the previous song in the list.
-    current_song_index = song_list.curselection()
-    prev_song_index = current_song_index[0] - 1 if current_song_index else 0
-    if prev_song_index >= 0:
-        song_list.selection_clear(0, 'end')
-        song_list.activate(prev_song_index)
-        song_list.selection_set(prev_song_index)
-        play_song()
-
 def update_progress():
-    #Update the progress label with the current song's progress.
+    # Function to update the progress label with the current song's progress.
     if pygame.mixer.music.get_busy():
         current_time = pygame.mixer.music.get_pos() // 1000
         minutes = int(current_time // 60)
@@ -144,6 +109,7 @@ def update_progress():
     else:
         progress_label["text"] = "Paused"
 
+    # If the song has finished playing, play the next song
     if not pygame.mixer.music.get_busy() and is_playing:
         play_next()
 
@@ -153,6 +119,31 @@ def select_song(event):
     # Event handler for selecting a song from the list.
     play_song()
 
+def play_next():
+    # Function to play the next song in the list or loop back to the first song.
+    next_song_index = song_list.curselection()[0] + 1
+    if next_song_index >= song_list.size():
+        next_song_index = 0  # Loop back to the first song
+
+    song_list.selection_clear(0, 'end')
+    song_list.activate(next_song_index)
+    song_list.selection_set(next_song_index)
+    play_song()
+
+def play_previous():
+    # Function to play the previous song in the list or play the last song if the current song is the first one.
+    current_song_index = song_list.curselection()
+    prev_song_index = current_song_index[0] - 1 if current_song_index else 0
+
+    if prev_song_index < 0:
+        prev_song_index = song_list.size() - 1  # Play the last song if the current song is the first one
+
+    song_list.selection_clear(0, 'end')
+    song_list.activate(prev_song_index)
+    song_list.selection_set(prev_song_index)
+    play_song()
+
+# Create the main window
 root = Tk()
 root.title("Music Player")
 root.geometry("330x500")  # Set the window size to 330x500, this size is suitable
@@ -184,7 +175,7 @@ scrollbar.pack(side="right", fill="y")
 song_list.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=song_list.yview)
 
-# Play/Resume Button
+# Load icons before running the application
 play_img = Image.open('play.png')
 play_img = play_img.resize((50, 50), resample=Image.BICUBIC)
 play_icon = ImageTk.PhotoImage(play_img)
@@ -193,30 +184,31 @@ pause_img = Image.open('pause.png')
 pause_img = pause_img.resize((50, 50), resample=Image.BICUBIC)
 pause_icon = ImageTk.PhotoImage(pause_img)
 
-play_resume_button = Button(root, image=play_icon, command=toggle_play)
-play_resume_button.grid(row=2, column=1, padx=10, pady=10)
-
-# Stop Button
 stop_img = Image.open('stop.png')
 stop_img = stop_img.resize((50, 50), resample=Image.BICUBIC)
 stop_icon = ImageTk.PhotoImage(stop_img)
 
-stop_button = Button(root, image=stop_icon, command=stop_song)
-stop_button.grid(row=2, column=2, padx=10, pady=10)
-
-# Next Button
 next_img = Image.open('next.png')
 next_img = next_img.resize((50, 50), resample=Image.BICUBIC)
 next_icon = ImageTk.PhotoImage(next_img)
 
-next_button = Button(root, image=next_icon, command=play_next)
-next_button.grid(row=2, column=3, padx=10, pady=10)
-
-# Previous Button
 prev_img = Image.open('previous.png')
 prev_img = prev_img.resize((50, 50), resample=Image.BICUBIC)
 prev_icon = ImageTk.PhotoImage(prev_img)
 
+# Play/Resume Button
+play_resume_button = Button(root, image=play_icon, command=toggle_play)
+play_resume_button.grid(row=2, column=1, padx=10, pady=10)
+
+# Stop Button
+stop_button = Button(root, image=stop_icon, command=stop_song)
+stop_button.grid(row=2, column=2, padx=10, pady=10)
+
+# Next Button
+next_button = Button(root, image=next_icon, command=play_next)
+next_button.grid(row=2, column=3, padx=10, pady=10)
+
+# Previous Button
 prev_button = Button(root, image=prev_icon, command=play_previous)
 prev_button.grid(row=2, column=0, padx=10, pady=10)
 
